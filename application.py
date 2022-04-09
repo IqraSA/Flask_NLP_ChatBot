1571f804ee21e919e0d599d44938d235204e9519
Iqra S Rao <iqra.saadia@gmail.com>
2022-04-09 21:24:38 +0800

    Update and rename app.py to application.py

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from chatbot import CB
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/chatbot")
+@application.route("/chatbot")
 def home():
     return render_template("index.html")
 
-@app.route("/get")
+@application.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     return str(CB.get_response(userText))
-app.run(debug = True)
+application.run(debug = True)
